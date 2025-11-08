794782806ade619d3cff4cfaabe6cd8cc27ed1ec
karthik.bhandary <karthik.bhandary@kfintech.com>
2025-11-08 09:02:52 +0000

    initial commit

---
+++ server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net"
+	pb "github.com/karthikbhandary2/go-grpc/proto"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8000"
+)
+
+type helloServer struct {
+	pb.GreetServiceServer 
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		panic(err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	log.Printf("server started at %v", lis.Addr())
+	if err = grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
